bd6dd64c8ef0b371147331f36542ec557b99d818
anhvd <anhvd27@gmail.com>
2020-01-02 10:54:07 +0700

    add return

--- viettel_shake/shake/views.py
+++ viettel_shake/shake/views.py
@@ -9,7 +9,7 @@ from .serializers import ShakeSerializer
 
 def view_404(request, exception=None):
     # make a redirect to homepage
-    redirect(reverse('shake:index'))
+    return redirect(reverse('shake:index'))
 
 
 class IndexTemplate(TemplateView):
