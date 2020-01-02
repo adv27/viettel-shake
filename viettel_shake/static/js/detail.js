9bfe6ff071df81ad10e72c9fc8462121b346c5fc
hahieu <goldenrezgun@gmail.com>
2020-01-02 15:42:43 +0700

    Style

--- viettel_shake/static/js/detail.js
+++ viettel_shake/static/js/detail.js
@@ -2,6 +2,23 @@ jQuery(function ($) {
     var $table = $('#table-history');
     var $refresh = $('#refresh');
 
+    function calData(shakes) {
+        console.log(shakes)
+
+        return [...shakes
+            .sort((a,b)=>a.data.status.code > b.data.status.code ? 1 : -1)
+            .map((shake)=>{
+            let res = {...shake};
+            if(shake.data.status.code !== "SG0023"){
+                res.data.status.code = shake.data.data.name;
+                console.log(shake.data.data)
+            }else {
+                res.data.status.code = 'FAIL';
+            }
+            return res;
+        })];
+    }
+
     var loadShakes = function (refresh = false) {
         if (refresh) toastr.warning('Đang cập nhật...');
 
@@ -11,11 +28,11 @@ jQuery(function ($) {
                 console.log(data);
                 if (refresh) {
                     // drop old rows then load new data
-                    $table.bootstrapTable('load', data.shakes);
+                    $table.bootstrapTable('load', calData(data.shakes));
                     toastr.success('Đã cập nhật!');
                 } else {
                     // init table
-                    $table.bootstrapTable({data: data.shakes});
+                    $table.bootstrapTable({data: calData(data.shakes)});
                 }
             })
             .catch(function (error) {
