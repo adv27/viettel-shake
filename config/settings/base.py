cc61791e38cb23cb73d67d0b78b798419f52f169
anhvd <anhvd27@gmail.com>
2020-01-26 15:16:29 +0700

    show recently gifts

--- config/settings/base.py
+++ config/settings/base.py
@@ -67,7 +67,7 @@ DJANGO_APPS = [
     "django.contrib.sites",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "django.contrib.humanize", # Handy template tags
+    "django.contrib.humanize",  # Handy template tags
     "django.contrib.admin",
 ]
 THIRD_PARTY_APPS = [
